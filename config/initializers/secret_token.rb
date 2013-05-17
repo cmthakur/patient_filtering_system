bdc920997901df46be071bb59364dd9b0c4fc987
Chandra <cmthakur.r2s@gmail.com>
2013-05-17 20:31:09 +0545

    Initialize the app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PatientFilteringSystem::Application.config.secret_token = '731b12c8959c6366286c0315e4bda6c22857e38e70fd56e5ea20cd12a07d657fe3ed88ee6027fd77aa0896fe5cdfd5c5605ef033f1f9d8c92d148e1eb9858d2d'
