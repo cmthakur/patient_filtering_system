685b174c65e0f2cbb5930373e7c4eb1f8a8e85c6
Chandra <cmthakur.r2s@gmail.com>
2013-05-19 09:05:43 +0545

    add controller and actions for patient

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,2 +1,22 @@
 class PatientsController < ApplicationController
+
+  def index
+    @patients = Patient.all
+  end
+
+  def new
+    @patient = Patient.new
+  end
+
+  def create
+    @patient = Patient.new(params['patient'])
+    respond_to do |format|
+      if @patient.save
+        format.html { redirect_to patients_path , notice: "Patient added successfully."}
+      else
+        format.html { render action: "new" }
+      end
+    end
+  end
+
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,4 +2,13 @@ class Patient < ActiveRecord::Base
   attr_accessible :fname, :mname, :lname, :email, :dob, :father_name, :mobile, :phone, :city, :country, :state
 
   has_many :trials
+  has_many :tests
+
+  def full_name
+    "#{fname} #{mname} #{lname}"
+  end
+
+  def address
+    "#{city} #{state} #{country}"
+  end
 end
