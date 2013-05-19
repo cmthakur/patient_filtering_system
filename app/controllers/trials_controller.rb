d74c30561333edf3af5e60b221bd76fad77d4fa4
Chandra <cmthakur.r2s@gmail.com>
2013-05-19 08:56:05 +0545

    add controller and actions for trials

--- app/controllers/trials_controller.rb
+++ app/controllers/trials_controller.rb
@@ -1,2 +1,26 @@
-class TrialsController < ApplicationController
+  class TrialsController < ApplicationController
+
+  def index
+    @trials = Trial.limit(50)
+  end
+
+  def new
+    @trial = Trial.new
+  end
+
+  def create
+    @trial = Trial.new(params['trial'])
+    respond_to do |format|
+      if @trial.save
+        format.html { redirect_to trials_path , notice: "Trail #{@trial.name} added successfully."}
+      else
+        format.html { render action: "new" }
+      end
+    end
+  end
+
+  def destroy
+    @trial = Trial.find params['id']
+    redirect_to trials_path , notice: "Trail #{@trial.name} removed successfully." if @trial.destroy
+  end
 end
